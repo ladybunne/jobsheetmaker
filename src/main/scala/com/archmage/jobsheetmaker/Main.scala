91ec80d06bbee19a5236598c6a936af3e7768b09
Rowan Walker <rowan.walker1@gmail.com>
2017-08-11 17:27:47 +1000

    Proof of concept of XLSX parsing implementation

--- src/main/scala/com/archmage/jobsheetmaker/Main.scala
+++ src/main/scala/com/archmage/jobsheetmaker/Main.scala
@@ -1,84 +1,44 @@
 package com.archmage.jobsheetmaker
 
-import java.time.LocalDate
-import java.time.Duration
-import com.archmage.jobsheetmaker.model.Job
-import com.archmage.jobsheetmaker.model.Worker
-import com.archmage.jobsheetmaker.model.Client
-import com.archmage.jobsheetmaker.model.Address
-import com.archmage.jobsheetmaker.model.JobList
-import java.io.File
-import java.time.LocalTime
-import java.time.temporal.ChronoUnit
-import com.opencsv.CSVReader
-import java.io.FileReader
-import scala.collection.mutable.ListBuffer
+import java.io.{FileInputStream, FileNotFoundException, IOException, InputStream}
+
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException
+import org.apache.poi.ss.usermodel.{CellType, WorkbookFactory}
 
 object Main extends App {
-
-	def read(filename: String, function: (Array[String]) => Unit) = {
-		val reader = new CSVReader(new FileReader(filename))
-		var nextLine = reader.readNext()
-		while (nextLine != null) {
-			function.apply(nextLine)
-			nextLine = reader.readNext()
+	var inp: InputStream = null
+	try {
+		inp = new FileInputStream("job_report.xlsx")
+		val workbook = WorkbookFactory.create(inp)
+		for(sheetIndex <- 0 until workbook.getNumberOfSheets) {
+			val sheet = workbook.getSheetAt(sheetIndex)
+			for(rowIndex <- 0 to sheet.getLastRowNum) {
+				val row = sheet.getRow(rowIndex)
+				for(cellIndex <- 0 until row.getLastCellNum) {
+					val cell = row.getCell(cellIndex)
+					cell.getCellTypeEnum match {
+						case CellType.NUMERIC => print(cell.getNumericCellValue)
+						case CellType.STRING => print(cell.getStringCellValue)
+						case CellType.BLANK => print("")
+						case _ => print(cell.getCellTypeEnum.name)
+					}
+					print("\t")
+				}
+				println()
+			}
 		}
-		reader.close
 	}
-
-	def items(input: Array[String]) = {
-		if (input(0) != "Name")
-			println(s"""[h1]${input(0)}[/h1]
-				|[i]${input(1)}[/i]
-				|[b]Description:[/b] ${input(2)}
-				|[b]Type:[/b] ${input(3)}${if (!input(4).isEmpty) s"\n[b]Notes:[/b] ${input(4)}" else ""}
-				|""".stripMargin)
+	catch {
+		case _: InvalidFormatException => {}
+		case _: FileNotFoundException => {}
+		case _: IOException => {}
 	}
-
-	def weapons(input: Array[String]) = {
-		if (input(0) != "Name")
-			println(s"""[h1]${input(0)}[/h1]
-				|[i]${input(1)}[/i]
-				|[b]Description:[/b] ${input(2)}
-				|[b]Damage:[/b] ${input(3)}
-				|[b]Range:[/b] ${input(4)}
-				|[b]Speed:[/b] ${input(5)}${if (!input(6).isEmpty) s"\n[b]Notes:[/b] ${input(6)}" else ""}
-				|""".stripMargin)
+	finally {
+		try {
+			inp.close()
+		}
+		catch {
+			case _: IOException => {}
+		}
 	}
-
-	def subweapons(input: Array[String]) = {
-		if (input(0) != "Name")
-			println(s"""[h1]${input(0)}[/h1]
-				|[i]${input(1)}[/i]
-				|[b]Description:[/b] ${input(2)}
-				|[b]Type:[/b] ${input(3)}
-				|[b]Cost:[/b] ${input(4)}${if (!input(5).isEmpty) s"\n[b]Notes:[/b] ${input(5)}" else ""}
-				|""".stripMargin)
-	}
-
-	def legacy(input: Array[String]) = {
-		if (input(0) != "Name")
-			println(s"""[h1]${input(0)}[/h1]
-				|[b]Description:[/b] ${input(1)}${if (!input(2).isEmpty) s"\n[b]Notes:[/b] ${input(2)}" else ""}
-				|""".stripMargin)
-	}
-
-	def characters(input: Array[String]) = {
-		if (input(0) != "Name")
-			println(s"""[h1]${input(0)}[/h1]
-				|[b]Hearts:[/b] ${input(1)}
-				|[b]Coins:[/b] ${input(2)}
-				|[b]Ammo:[/b] ${input(3)}
-				|[b]Keys:[/b] ${input(4)}
-				|[b]Bombs:[/b] ${input(5)}
-				|[b]Weapon:[/b] ${input(6)}
-				|[b]Items:[/b] ${input(7)}${if (!input(8).isEmpty) s"\n[b]Notes:[/b] ${input(8)}" else ""}
-				|""".stripMargin)
-	}
-
-	read("items.csv", items)
-	read("weapons.csv", weapons)
-	read("subweapons.csv", subweapons)
-	read("legacy.csv", legacy)
-	read("characters.csv", characters)
 }
